1774f8af33ee1d01a67504e03b6d7d2e1f32de45
guilherme2506 <guilherme.augd@gmail.com>
2024-01-05 11:47:04 -0300

    updated onclick property inside the onclick function

--- 01_dragon-repeller-game/script.js
+++ 01_dragon-repeller-game/script.js
@@ -27,7 +27,13 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
     button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square.";
+    button3.innerText = "Go to town square";
+
+    //set onclick property for each button
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+
 }
 
 function goCave() {
