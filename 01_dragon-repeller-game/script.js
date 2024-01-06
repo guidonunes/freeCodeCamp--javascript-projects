35f5c6fe92b4348245926fb1118409c5a37ff9c6
guilherme2506 <guilherme.augd@gmail.com>
2024-01-06 15:04:26 -0300

    declared const weapons & assigned array objects

--- 01_dragon-repeller-game/script.js
+++ 01_dragon-repeller-game/script.js
@@ -22,6 +22,25 @@ const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+    {
+        name: "stick",
+        power: 5,
+    },
+    {
+        name: "dagger",
+        power: 30,
+    },
+    {
+        name: "claw hammer",
+        power: 50
+    },
+    {
+        name: "sword",
+        power: 100, 
+    }
+]; 
+
 const locations = [
     {
     name: "town square",
