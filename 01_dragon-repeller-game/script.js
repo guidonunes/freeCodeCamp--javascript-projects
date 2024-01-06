88c2a7209458367e9bad2c58d4d3928c13aef9cb
guilherme2506 <guilherme.augd@gmail.com>
2024-01-06 15:28:50 -0300

    added code to the buyWeapon func

--- 01_dragon-repeller-game/script.js
+++ 01_dragon-repeller-game/script.js
@@ -108,6 +108,13 @@ function buyHealth() {
 }
 
 function buyWeapon() {
+    if(gold >= 30) {
+       gold -= 30;
+       currentWeapon++;
+       goldText.innerText = gold;
+       let newWeapon = weapons[currentWeapon].name;
+       text.innerText = "You now have a " + newWeapon + ".";
+    }
 
 }
 
