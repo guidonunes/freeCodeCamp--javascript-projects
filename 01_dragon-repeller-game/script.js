c9a2c729d73f6c69a5b8209155aaf9f116abd934
guilherme2506 <guilherme.augd@gmail.com>
2024-01-08 18:05:31 -0300

    created monsters const & assigned array of obj

--- 01_dragon-repeller-game/script.js
+++ 01_dragon-repeller-game/script.js
@@ -41,6 +41,24 @@ const weapons = [
     }
 ]; 
 
+const monsters = [
+    {
+        name:"slime",
+        level: 2,
+        health:15,
+    },
+    {
+        name:"fanged beast",
+        level: 8 ,
+        health:60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health:300
+    }
+]
+
 const locations = [
     {
     name: "town square",
@@ -92,9 +110,7 @@ function goCave() {
     update(locations[2]);
 }
 
-function fightDragon() {
-    console.log("Fighting dragon.");
-}
+
 
 function buyHealth() {
    if (gold >= 10) {
@@ -145,6 +161,10 @@ function fightSlime(){
 
 function fightBeast() {
 
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.");
 }
 
 
