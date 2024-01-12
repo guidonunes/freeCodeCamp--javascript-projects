cf257af8be7c583da01a70cba23f4eef9ad98095
guilherme2506 <guilherme.augd@gmail.com>
2024-01-12 10:02:57 -0300

    updated if statement at the end of the attack function

--- 01_dragon-repeller-game/script.js
+++ 01_dragon-repeller-game/script.js
@@ -195,7 +195,7 @@ function attack() {
     fighting === 2 ? winGame() : defeatMonster();
   }
   if (Math.random() <= .1) {
-    
+    text.innerText += " Your " + inventory.pop() + " breaks."
   }
 }
 
