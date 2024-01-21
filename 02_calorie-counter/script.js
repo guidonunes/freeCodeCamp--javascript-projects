a1d86141ec1d1bc64fb5c66cfdfee0af04b58a46
guilherme2506 <guilherme.augd@gmail.com>
2024-01-21 17:38:42 -0300

    called the .addEventListener method of the addEntryButton

--- 02_calorie-counter/script.js
+++ 02_calorie-counter/script.js
@@ -27,3 +27,5 @@ function addEntry() {
     <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"/>`;
     targetInputContainer.innerHTML += HTMLString;
 }   
+
+addEntryButton.addEventListener('click', addEntry());
