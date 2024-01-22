d2bd01cf8707769c0bf05a3fa929ed3f1f419080
guilherme2506 <guilherme.augd@gmail.com>
2024-01-22 11:59:57 -0300

    declared invalidInputMatch variable within the for loop in the getCaloriesFromInputs function

--- 02_calorie-counter/script.js
+++ 02_calorie-counter/script.js
@@ -31,7 +31,8 @@ function addEntry() {
 function getCaloriesFromInputs(list) {
     let calories = 0;
     for(let i = 0; i < list.length; i++){
-        let currVal = cleanInputString(list[i].value);
+        const currVal = cleanInputString(list[i].value);
+        const isInvalidInputMatch = isInvalidInput(currentVal);
     }
 }
 
