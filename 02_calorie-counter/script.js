3ac4b47982ad84e5a2a8632c0c449a96821e54cb
guilherme2506 <guilherme.augd@gmail.com>
2024-01-23 11:33:47 -0300

    declared budgetCalories variable

--- 02_calorie-counter/script.js
+++ 02_calorie-counter/script.js
@@ -42,6 +42,8 @@ function calculateCalories(e) {
     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 }
 
 function getCaloriesFromInputs(list) {
