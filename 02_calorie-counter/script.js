e944494143b86047b2bd9f64da55a690df8054ca
guilherme2506 <guilherme.augd@gmail.com>
2024-01-23 15:41:15 -0300

    added an hr element after the span element

--- 02_calorie-counter/script.js
+++ 02_calorie-counter/script.js
@@ -53,7 +53,10 @@ function calculateCalories(e) {
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories >= 0 ? 'Surplus': 'Deficit';
 
-    output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>`;
+    output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    `;
 }   
     
 function getCaloriesFromInputs(list) {
