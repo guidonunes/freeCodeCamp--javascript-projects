7d6383cf339f7d7e22acebae1412c7a03e2dbd3b
guilherme2506 <guilherme.augd@gmail.com>
2024-01-23 11:27:18 -0300

    declare variables for the number inputs in the #dinner, #snacks and #exercise elements

--- 02_calorie-counter/script.js
+++ 02_calorie-counter/script.js
@@ -38,6 +38,10 @@ function calculateCalories(e) {
     const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
 
     const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
 }
 
 function getCaloriesFromInputs(list) {
