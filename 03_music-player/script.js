dd763dd733d3e50ac8797f36b44a0ac50935428e
guilherme2506 <guilherme.augd@gmail.com>
2024-01-24 14:21:22 -0300

    added allSongs variable & assigned an empty array to it

--- 03_music-player/script.js
+++ 03_music-player/script.js
@@ -5,3 +5,4 @@ const nextButton = document.getElementById('next');
 const previousButton = document.getElementById('previous');
 const shuffleButton = document.getElementById('shuffle');
 
+const allSongs = [];
