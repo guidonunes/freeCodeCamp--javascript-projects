d5bea6163011e9eb8ff5544f53c73f6b648939ac
guilherme2506 <guilherme.augd@gmail.com>
2024-01-28 09:43:12 -0300

    added an else statement to the if block within playButton event listener

--- 03_music-player/script.js
+++ 03_music-player/script.js
@@ -125,6 +125,8 @@ const renderSongs = (array) => {
 playButton.addEventListener('click', () => {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id)
+  } else {
+    playSong(userData?.currentSong.id);
   }
 })
 
