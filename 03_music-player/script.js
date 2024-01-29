f87f521bdddb6e3c6798cbf24b58a06d4cc52881
guilherme2506 <guilherme.augd@gmail.com>
2024-01-29 17:26:34 -0300

    added avent listener to previousButton element

--- 03_music-player/script.js
+++ 03_music-player/script.js
@@ -162,4 +162,5 @@ playButton.addEventListener('click', () => {
 
 pauseButton.addEventListener('click', pauseSong);
 nextButton.addEventListener('click', playNextSong);
+previousButton.addEventListener('click', playPreviousSong);
 renderSongs(userData?.songs);
