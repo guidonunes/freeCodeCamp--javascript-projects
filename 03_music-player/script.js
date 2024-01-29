3604da86af35119be996246fdab071963825a09d
guilherme2506 <guilherme.augd@gmail.com>
2024-01-29 17:55:34 -0300

    used removeAttribute method within the callback function of playlistSongElements constant

--- 03_music-player/script.js
+++ 03_music-player/script.js
@@ -131,7 +131,7 @@ const highlightCurrentSong  = () => {
   const playlistSongElements = document.querySelectorAll('.playlist-song')
   const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`)
   playlistSongElements.forEach((songEl) => {
-    
+    songEl.removeAttribute("aria-current");
   })
 }
 
