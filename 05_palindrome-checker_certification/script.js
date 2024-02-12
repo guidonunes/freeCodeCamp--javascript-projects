a34096adb31a440572335584c7c68307db71d951
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-12 18:14:00 -0300

    implemented switch statement

--- 05_palindrome-checker_certification/script.js
+++ 05_palindrome-checker_certification/script.js
@@ -1,9 +1,19 @@
 const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
-const result = document.getElementById('result')
+const result = document.getElementById('result');
 
 checkBtn.addEventListener('click', () => {
     if(textInput.value.trim() === '') {
         alert('Please input a value')
+    } else {
+        const inputValue = textInput.value.trim().toLowerCase();
+        switch (inputValue) {
+            case 'a':
+                result.textContent = 'A is a palindrome';
+                break;
+            default:
+                result.textContent = '';
+                break;
+        }
     }
-})
+});
