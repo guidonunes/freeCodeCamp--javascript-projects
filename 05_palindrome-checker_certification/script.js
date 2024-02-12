f65987286d0ef7b9502efa955a47d65c2288f1d3
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-12 17:56:46 -0300

    add const variable result

--- 05_palindrome-checker_certification/script.js
+++ 05_palindrome-checker_certification/script.js
@@ -1,2 +1,8 @@
 const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
+
+checkBtn.addEventListener('click', () => {
+    if(textInput.value.trim() === '') {
+        alert('Please input a value')
+    }
+})
