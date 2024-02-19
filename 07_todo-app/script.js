8ab5424e0fd3803082058d86d5f7f7e0285cb99c
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-19 15:01:53 -0300

    implement splice method on taskData & remove method on buttonEl

--- 07_todo-app/script.js
+++ 07_todo-app/script.js
@@ -48,6 +48,8 @@ const updateTaskContainer = () => {
 
 const deleteTask = (buttonEl) => {
 const dataArrIndex = taskData.findIndex(item => item.id === buttonEl.parentElement.id)
+buttonEl.parentElement.remove();
+taskData.splice(dataArrIndex, 1);
 }
 
 const reset = () => {
