2329ba70b0c328bd76069d93422842d45d2e39c3
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-19 15:42:05 -0300

    set the value of dateInput to currentTask.date

--- 07_todo-app/script.js
+++ 07_todo-app/script.js
@@ -56,6 +56,7 @@ const editTask = (buttonEl) => {
     const dataArrIndex = taskData.findIndex(item => item.id === buttonEl.parentElement.id)
     currentTask = taskData[dataArrIndex];
     titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
     
 }
 
