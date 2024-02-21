4073e34c05d2f998a11f858106d0e389f9bde3f4
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-21 14:37:23 -0300

    implement localStorage.clear method

--- 07_todo-app/script.js
+++ 07_todo-app/script.js
@@ -120,7 +120,7 @@ const myTaskArr = [
   ];
 
   localStorage.setItem("data", JSON.stringify(myTaskArr));
-  
+  localStorage.clear()
 
   const getTaskArr = localStorage.getItem("data");
   const getTaskArrObj = JSON.parse(localStorage.getItem(myTaskArr));
