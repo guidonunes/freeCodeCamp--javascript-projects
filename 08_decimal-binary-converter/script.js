8916fc78a6d1e79cb11531545f46e5a4562dc26a
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-25 17:37:09 -0300

    add console log a()

--- 08_decimal-binary-converter/script.js
+++ 08_decimal-binary-converter/script.js
@@ -9,6 +9,9 @@ const b = () => {
 const c = () => {
     return "awesome!"
 }
+
+console.log(a());
+
 const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
