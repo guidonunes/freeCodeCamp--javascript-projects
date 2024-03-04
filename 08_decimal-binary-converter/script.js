bc9ddc0a4722635329ed077d5e150b90e1291503
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-04 16:40:16 -0300

    add variable & assign getElementById method

--- 08_decimal-binary-converter/script.js
+++ 08_decimal-binary-converter/script.js
@@ -1,6 +1,7 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById('animation-container');
 
     //array of objects to store data for each frame of the animation
     const animationData = [
