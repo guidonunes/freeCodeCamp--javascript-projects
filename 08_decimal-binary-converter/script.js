074959d1d805b8b943ae21c4b0ff2df60d1f4bde
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-04 20:29:01 -0300

    add a style attribute

--- 08_decimal-binary-converter/script.js
+++ 08_decimal-binary-converter/script.js
@@ -38,7 +38,7 @@ const showAnimation = () => {
     animationData.forEach((obj) => {
     // set up the delay to add elements to the DOM    
         setTimeout(() => {
-            animationContainer.innerHTML += `<p id=''></p>`;
+            animationContainer.innerHTML += `<p id='${inputVal}' style = 'margin-top:${obj.marginTop}px;'></p>`;
         }, obj.addElDelay)
     })
 };
