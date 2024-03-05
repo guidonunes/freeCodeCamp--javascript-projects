dcb09455a2c9bd911b2148028f6303dbe2fef7f4
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-04 21:09:55 -0300

    add variable and assign getElementById method to it

--- 08_decimal-binary-converter/script.js
+++ 08_decimal-binary-converter/script.js
@@ -54,7 +54,8 @@ const showAnimation = () => {
 
     //update the paragraphs with the msg text
         setTimeout(() => {
-
+    // select the element with the id attribute with the value of the inputVal property of the current object.
+            let paragraphElement = document.getElementById(obj.inputVal);
         }, obj.showMsgDelay);
     });
 };
