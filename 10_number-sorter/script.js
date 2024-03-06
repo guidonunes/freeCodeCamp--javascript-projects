c7bfd6efa53c2059a768b75671f36f6f579d628a
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-06 15:30:49 -0300

    call preventDefault method on event

--- 10_number-sorter/script.js
+++ 10_number-sorter/script.js
@@ -2,5 +2,6 @@ const sortButton = document.getElementById('sort');
 
 
 const sortInputArray = (event) => {
-
+    //avoid default submission of the numbers associated with the form element
+    event.preventDefault();
 }
