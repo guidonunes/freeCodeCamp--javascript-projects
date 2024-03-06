feba24038943aaa1e107b80068cb1def2720768c
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-06 16:36:23 -0300

    add variable to call back function

--- 10_number-sorter/script.js
+++ 10_number-sorter/script.js
@@ -13,7 +13,7 @@ const sortInputArray = (event) => {
 
 const updateUI = (array = []) => {
     array.forEach((num, i) => {
-
+        const outputValueNode = document.getElementById(`output-value-${i}`);
     });
 }
 
