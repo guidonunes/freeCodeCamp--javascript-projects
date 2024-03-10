b0b40dc80b98d6d61abe50d59f602971738b76ba
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-10 15:39:34 -0300

    add variable & update argument passed into updateUI function call

--- 10_number-sorter/script.js
+++ 10_number-sorter/script.js
@@ -7,7 +7,10 @@ const sortInputArray = (event) => {
 
     //convert the input values to an array by using the spread operator
     const inputValues = [...document.getElementsByClassName('values-dropdown')].map((dropdown) => Number(dropdown.value));
-    updateUI(inputValues);
+    const sortedValues = bubbleSort(inputValues)
+
+    updateUI(sortedValues);
+
 }
 
 //display the sorted array on the screen
