81dc516b91920c9d1d41810ff7970f9808dd3c76
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-17 09:36:06 -0300

    add switch statement and switch case

--- 09_roman-n-converter_certification/script.js
+++ 09_roman-n-converter_certification/script.js
@@ -3,6 +3,12 @@ const numberInput = document.getElementById('number');
 const output = document.getElementById('output');
 
 convertBtn.addEventListener("click", () =>{
-    const inputValue = parseInt(numberInput.value.trim())
+    const inputValue = parseInt(numberInput.value.trim());
+
+    switch(true) {
+        case inputValue === -1:
+            output.textContent = "Please enter a number greater than or equal to 1."
+            break;
+    }
 })
 
