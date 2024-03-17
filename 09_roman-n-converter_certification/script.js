fd8b36f734deca4fb4380ce6e612af6cd8a9bb82
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-17 10:16:12 -0300

    add array of objects

--- 09_roman-n-converter_certification/script.js
+++ 09_roman-n-converter_certification/script.js
@@ -2,9 +2,28 @@ const convertBtn = document.getElementById('convert-btn');
 const numberInput = document.getElementById('number');
 const output = document.getElementById('output');
 
+const numerals = [
+    {char:"M", value: 1000},
+    {char:"CM", value: 900},
+    {char:"D", value: 500},
+    {char:"CD", value: 400},
+    {char:"C", value: 100},
+    {char:"XC", value: 90},
+    {char:"L", value: 50},
+    {char:"XL", value: 40},
+    {char:"X", value: 10},
+    {char:"IX", value: 9},
+    {char:"V", value: 5},
+    {char:"IV", value: 4},
+    {char:"I", value: 1},
+]
+
+
+
+
 convertBtn.addEventListener("click", () =>{
     const inputValue = parseInt(numberInput.value.trim());
-
+//Check if the input is valid or not
     switch(true) {
         case numberInput.value.trim() === "":
             output.textContent = "Please enter a valid number";
