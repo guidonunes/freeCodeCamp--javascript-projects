a501bd618188793a0b108e7aeddfdeef80bf4f8f
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-17 09:25:12 -0300

    call add event listener on convertBtn

--- 09_roman-n-converter_certification/script.js
+++ 09_roman-n-converter_certification/script.js
@@ -2,10 +2,7 @@ const convertBtn = document.getElementById('convert-btn');
 const numberInput = document.getElementById('number');
 const output = document.getElementById('output');
 
-
-
-convertBtn.addEventListener("click", () => {
-    if (convertBtn === " "){
-        alert("Please")
-    }
+convertBtn.addEventListener("click", () =>{
+    
 })
+
