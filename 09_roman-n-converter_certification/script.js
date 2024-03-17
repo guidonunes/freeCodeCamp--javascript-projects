0971fe0441f3917be42d605bb2a5b693f0c43d16
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-17 09:16:25 -0300

    add const variables to get id

---
+++ 09_roman-n-converter_certification/script.js
@@ -0,0 +1,11 @@
+const convertBtn = document.getElementById('convert-btn');
+const number = document.getElementById('number');
+
+
+
+
+convertBtn.addEventListener("click", () => {
+    if (convertBtn === " "){
+        alert("Please")
+    }
+})
