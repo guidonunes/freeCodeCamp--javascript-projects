a71327f1d670f62bbc4801f53f7097fcfb1eb630
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-19 15:26:08 -0300

    add const variable storing numerals keys & values

--- 09_roman-n-converter_certification/script.js
+++ 09_roman-n-converter_certification/script.js
@@ -6,7 +6,21 @@ const output = document.getElementById('output');
 
 //conversion function for valid input
 const convertToRoman = (num) => {
-//TODO: Add conversion function 
+    const numerals = {
+        M: 1000, 
+        CM: 900, 
+        D: 500, 
+        CD: 400, 
+        C: 100, 
+        XC: 90, 
+        L: 50, 
+        XL: 40, 
+        X: 10, 
+        IX: 9, 
+        V: 5, 
+        IV: 4, 
+        I: 1,
+    };
 };
 
 
