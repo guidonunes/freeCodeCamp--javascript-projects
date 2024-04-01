554f43f8c248e32fd6bf00253e423962e2617d55
Guilherme Nunes <guilherme.augd@gmail.com>
2024-03-31 21:51:35 -0300

    move code to correct place

--- 11_statistics-calculator/script.js
+++ 11_statistics-calculator/script.js
@@ -4,6 +4,10 @@ const getMean = (array) => array.reduce((acc,el)=>acc +el, 0)/array.length;
 //calculate median logic
 const getMedian = (array) => {
     const sorted = array.sort((a,b) => a - b);
+    const median = array.length %2 === 0 ?
+    getMean([sorted[array.length/ 2], sorted[array.length/ 2 -1]]) :
+    sorted[Math.floor(array.length/2)];
+    return median;
 }
 
 const calculate = () => {
@@ -11,9 +15,6 @@ const calculate = () => {
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
     const mean = getMean(numbers);
+    const median = getMedian(numbers);
     document.querySelector('#mean').textContent = mean;
-    const median = array.length %2 === 0 ?
-    getMean([sorted[array.length/ 2], sorted[array.length/ 2 -1]]) :
-    sorted[Math.floor(array.length/2)];
-    return median;
 }
