d284ad1bf3aed85698fa6b8f46c838b2a23bd816
Guilherme Nunes <guilherme.augd@gmail.com>
2024-04-03 15:00:27 -0300

    implement if statement

--- 11_statistics-calculator/script.js
+++ 11_statistics-calculator/script.js
@@ -17,6 +17,10 @@ const getMode = (array) => {
     array.forEach((el) => {
         counts[el] = (counts[el] || 0) + 1;
     });
+
+    if (new Set(Object.values(counts)).size === 1) {
+        return null;
+    }
 }
 
 const calculate = () => {
