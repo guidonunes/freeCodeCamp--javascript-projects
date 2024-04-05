0dd82d9197a378e042b62625e973c3322cf0ec44
Guilherme Nunes <guilherme.augd@gmail.com>
2024-04-04 21:38:18 -0300

    add function and return the difference of numbers

--- 11_statistics-calculator/script.js
+++ 11_statistics-calculator/script.js
@@ -13,23 +13,25 @@ const getMedian = (array) => {
 //get the number that appears most often in the list
 const getMode = (array) => {
     const counts = {}
-
     array.forEach((el) => {
         counts[el] = (counts[el] || 0) + 1;
     });
-
     if (new Set(Object.values(counts)).size === 1) {
         return null;
     }
     const highest = Object.keys(counts).sort((a,b) => {
         return counts[b] - counts[a]
     })[0];
-
     const mode = Object.keys(counts).filter((el) => counts[el] === counts[highest]);
-
     return mode.join(", ");
 }
 
+//calculate range
+const getRange = (array) => {
+    return Math.max(...array) - Math.min(...array)
+}
+
+//calculate
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
