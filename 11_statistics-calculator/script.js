7b89bbca552623aa9bd957b33fde1c6496c8cc54
Guilherme Nunes <guilherme.augd@gmail.com>
2024-04-21 21:42:34 -0300

    add return statement

--- 11_statistics-calculator/script.js
+++ 11_statistics-calculator/script.js
@@ -45,6 +45,7 @@ const getVariance = (array) => {
 const getStandardDeviation = (array) => {
     const variance = getVariance(array);
     const standardDeviation = Math.sqrt(variance);
+    return standardDeviation;
 }
 
 //calculate
