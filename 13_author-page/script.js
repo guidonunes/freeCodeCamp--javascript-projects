af67167a9264c7d8235c27e22816cda14bb9636c
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-08 21:38:35 -0300

    assign data to authorDataArr variable

--- 13_author-page/script.js
+++ 13_author-page/script.js
@@ -8,7 +8,7 @@ let authorDataArr = [];
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
   .then((res) => res.json())
   .then((data) => {
-
+    const authorDataArr = data
   })
   .catch((err) => console.error(`There was an error: ${err}`))
 
