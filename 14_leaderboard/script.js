70f5b782679d7fa2490042bc430e9e21e9a3e126
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-18 22:29:53 -0300

    assign links to variables

--- 14_leaderboard/script.js
+++ 14_leaderboard/script.js
@@ -1,2 +1,4 @@
 const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl =
