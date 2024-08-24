892244f4719acc73843c8e21713a2bb0a5608523
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-24 17:22:32 -0300

    add event listener and get id of results-div

--- 15_us-phone-validator_certification/script.js
+++ 15_us-phone-validator_certification/script.js
@@ -1,3 +1,24 @@
 const userInput = document.getElementById('user-input');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
+const resultsDiv = document.getElementById('results-div');
+
+
+
+const checkPhone = () => {
+  if (userInput.value === '') {
+    alert('Please provide a phone number');
+  } else {
+    const regEx = /^1?(\d{3})(\d{3})(\d{4})$/;
+    const validPhone = regEx.test(userInput.value);
+
+    if (validPhone) {
+      userInput.style.borderColor = 'green';
+    } else {
+      userInput.style.borderColor = 'red';
+    }
+  }
+
+};
+
+checkBtn.addEventListener('click', checkPhone);
