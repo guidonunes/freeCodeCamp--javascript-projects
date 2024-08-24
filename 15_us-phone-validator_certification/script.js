234031c670c907b9c3719df4e586eab32e1549e1
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-24 17:49:02 -0300

    refactor if/else statement and moved clearBtn event listener function

--- 15_us-phone-validator_certification/script.js
+++ 15_us-phone-validator_certification/script.js
@@ -11,16 +11,15 @@ const checkPhone = () => {
     const regEx = /^1?\s?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
     const validPhone = regEx.test(inputVal);
 
-    if (validPhone) {
-      resultsDiv.innerHTML = `<p class="text-success">Valid US number: ${inputVal}</p>`;
-    } else {
-      resultsDiv.innerHTML = '<p class="text-danger">Invalid US phone number</p>';
-    }
+    validPhone ?
+      resultsDiv.innerHTML = `<p class="text-success">Valid US number: ${inputVal}</p>` :
+      resultsDiv.innerHTML = `<p class="text-danger">Invalid US number: ${inputVal}</p>`;
   }
 };
 
-checkBtn.addEventListener('click', checkPhone);
 clearBtn.addEventListener('click', () => {
   userInput.value = '';
   resultsDiv.innerHTML = '';
 });
+
+checkBtn.addEventListener('click', checkPhone);
