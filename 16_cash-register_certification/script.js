bd7ca41ce179b56b8f046560afdd6d37f39e52b6
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-25 09:04:13 -0300

    add variables for price and cash register

--- 16_cash-register_certification/script.js
+++ 16_cash-register_certification/script.js
@@ -1,3 +1,17 @@
 const changeDue = document.getElementById('change-due');
 const cash = document.getElementById('cash');
 const purchaseBtn = document.getElementById('purchase-btn');
+
+
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
