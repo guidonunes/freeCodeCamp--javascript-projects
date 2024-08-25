75f2da2a5a2a0bd54c3d1bbc6f832087149c191c
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-25 09:46:01 -0300

    add cash drawer display function

--- 16_cash-register_certification/script.js
+++ 16_cash-register_certification/script.js
@@ -6,7 +6,7 @@ const cashDrawerDisplay = document.getElementById('cash-drawer-display');
 
 
 
-let price = 1.87;
+let price = 3.87;
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -19,9 +19,22 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
+const displayCashDrawer = () => {
+  priceScreen.innerHTML = `<h3>Price: $${price.toFixed(2)}</h3>`;
+
+  // Display cash drawer
+  cashDrawerDisplay.innerHTML = '<h3>Cash Drawer Contents: </h3>';
+  cid.forEach(([denomination, amount]) => {
+    cashDrawerDisplay.innerHTML += `<p>${denomination}: $${amount.toFixed(2)}</p>`;
+  });
+};
+
 const checkCashRegister = (price, cash, cid) => {
   if (price > cash) {
     changeDue.innerHTML = '<p class="text-danger">Customer does not have enough money to purchase the item</p>';
     return;
   }
 }
+
+
+displayCashDrawer();
