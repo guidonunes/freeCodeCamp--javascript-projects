a57ac97aeaab3e052cff6c1abf9c76a6b8263d7e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-28 15:50:41 -0300

    add function to sanitize search input entries

--- 17_pokemon-search-app/script.js
+++ 17_pokemon-search-app/script.js
@@ -5,7 +5,10 @@ const searchInput = document.getElementById('search-input');
 const proxyApi ='https://pokeapi-proxy.freecodecamp.rocks/api/pokemon';
 
 
-
+// Function to sanitize user input
+const sanitizeInput = (input) => {
+  return input.toLowerCase().replace(/[^a-z0-9]/g, '');
+}
 
 const searchPokemon = async () => {
   try {
