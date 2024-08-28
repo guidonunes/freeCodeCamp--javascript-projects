88b2396b65803c294de7443cb245fcd9b212fc08
Guilherme Nunes <guilherme.augd@gmail.com>
2024-08-28 16:17:24 -0300

    got elements by id

--- 17_pokemon-search-app/script.js
+++ 17_pokemon-search-app/script.js
@@ -1,5 +1,17 @@
 const searchInput = document.getElementById('search-input');
-
+const searchButton = document.getElementById('search-button');
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonId = document.getElementById('pokemon-id');
+const weightElement = document.getElementById('weight');
+const heightElement = document.getElementById('height');
+const spriteContainer = document.getElementById('sprite-container');
+const typesElement = document.getElementById('types');
+const hpElement = document.getElementById('hp');
+const attackElement = document.getElementById('attack');
+const defenseElement = document.getElementById('defense');
+const specialAttackElement = document.getElementById('special-attack');
+const specialDefenseElement = document.getElementById('special-defense');
+const speedElement = document.getElementById('speed');
 
 
 const proxyApi ='https://pokeapi-proxy.freecodecamp.rocks/api/pokemon';
@@ -12,12 +24,25 @@ const sanitizeInput = (input) => {
 
 const searchPokemon = async () => {
   try {
-    const response = await fetch(`${proxyApi}/${searchInput.value}`);
+    const sanitizedInput = sanitizeInput(searchInput.value);
+
+    const response = await fetch(`${proxyApi}/${sanitizedInput}`);
+
+    if (!response.ok) {
+      alert('Pokemon not found');
+      return;
+    }
+
     const data = await response.json();
-    console.log(data);
+
+    if(!data|| Object.keys(data).length === 0) {
+      alert('Pokemon not found');
+    } else {
+      console.log(data);
+    }
   } catch (error) {
-    console.log(error);
+    console.error('Error fetching Pokémon data:', error);
   }
 }
 
-searchPokemon();
+searchInput.addEventListener('input', searchPokemon);
